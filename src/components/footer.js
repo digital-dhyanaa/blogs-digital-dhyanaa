3a5ac85515d29978bf5a06c6c79f01d1227a8084
Ruchit Patel <rpruchitpatel14@gmail.com>
2024-04-12 16:48:29 +0530

    Google analytics support

--- src/components/footer.js
+++ src/components/footer.js
@@ -12,11 +12,11 @@ const Footer = () => (
   >
     <div className="container">
       <p>
-        A GatsbyJS Starter for Netlify CMS, Made with{" "}
+        Blogs for Digital Dhyanaa, Made with{" "}
         <span className="icon -love">
           <RiHeart2Line />
         </span>{" "}
-        by <Link to="/">Stackrole.com</Link>
+        by <Link to="https://sayajiinfotech.com">Sayaji Infotech</Link>
       </p>
     </div>
   </footer>
