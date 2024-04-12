fa25cab7c39393a11bdb1802ed417be9635def00
Ruchit Patel <rpruchitpatel14@gmail.com>
2024-04-12 17:54:08 +0530

    Google analytics support with gtag plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,10 +89,23 @@ module.exports = {
     "gatsby-plugin-theme-ui",
     `gatsby-plugin-netlify-cms`,
     {
-      resolve: `gatsby-plugin-google-analytics`,
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: settings.ga,
-      },
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          settings.ga, // Google Analytics / GA
+        ],
+
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      }
     },
     `gatsby-plugin-sitemap`,
     {
